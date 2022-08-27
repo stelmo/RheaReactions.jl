785e261f56a2bf1119bce3349e13ae889a80fa90
St. Elmo Wilken <stelmozors@gmail.com>
2022-08-27 21:06:40 +0200

    clear cache for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using RheaReactions
 using Test
 
 @testset "RheaReactions.jl" begin
+    clear_cache!()
     include("reactions.jl")
     include("metabolites.jl")
     include("reaction_matches.jl")
